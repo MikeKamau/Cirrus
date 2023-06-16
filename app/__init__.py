e83345b7a736723b26c847e203e5b7f688c76e0d
mikekamau <11148811+MikeKamau@users.noreply.github.com>
2023-06-16 12:12:28 +0300

    Rename log file

--- app/__init__.py
+++ app/__init__.py
@@ -53,7 +53,7 @@ if not app.debug:
     if not os.path.exists("logs"):
         os.mkdir("logs")
     file_handler = RotatingFileHandler(
-        "logs/babyapp.log", maxBytes=10240, backupCount=10
+        "logs/cirrus.log", maxBytes=10240, backupCount=10
     )
     file_handler.setFormatter(
         logging.Formatter(
